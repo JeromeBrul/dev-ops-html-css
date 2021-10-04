96438052023590babd6784fccce04dd5294ff595
brulard <contact.brulard@gmail.com>
2021-10-04 09:43:52 +0200

    pp

---
+++ js/06-objet/js/main.js
@@ -0,0 +1,9 @@
+var personnage1 = new Object();
+personnage1.name = "Aragorne";
+personnage1.house = "Gondor";
+personnage1.weapon = "épée";
+
+console.log(personnage1);
+console.log(personnage1.name);
+console.log(personnage1["name"]); // cette version est obligatoire pour les boucles
+
